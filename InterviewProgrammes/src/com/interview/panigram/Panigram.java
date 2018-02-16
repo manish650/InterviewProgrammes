d7fd151e24f4a5b3390f357cbd42838336fcc16c
Manish Mishra <manishmishra650@gmail.com>
2018-02-16 12:41:16 +0530

    final commit

--- InterviewProgrammes/src/com/interview/panigram/Panigram.java
+++ InterviewProgrammes/src/com/interview/panigram/Panigram.java
@@ -2,27 +2,32 @@ package com.interview.panigram;
 
 public class Panigram {
 	public static void main(String[] args) {
-		String input="abfdhjklfeis";
-		boolean result=Panigram.searchPanigram(input);
+		String input = "asdfghjklzxcvbnmwertyuiopq";
+		boolean result = Panigram.searchPanigram(input);
 		System.out.println(result);
 	}
+
 	public static boolean searchPanigram(String input) {
-		input=input.toLowerCase();
-		int[] out=new int[26];
-		char[] inputChar=input.toCharArray();
-		for(int i=0;i<input.length();i++)
-		{
-			if(inputChar[i]>='a'&&inputChar[i]<='z')
-			{
-				if(out[(inputChar[i]-'a')]<1) {
-				++out[(inputChar[i]-'a')];
-				}
-			    else 
-			    	return false; 
+		input = input.toLowerCase();
+		int[] out = new int[26];
+		//boolean flag=true;
+		char[] inputChar = input.toCharArray();
+		int k = 0, l = 0,m=0;
+		for (int i = 0; i < input.length(); i++) {
+			if (inputChar[i] >= 'a' && inputChar[i] <= 'z') {
+				k++;
+				l = (inputChar[i] - 'a');
+				if (out[l] < 1)
+					out[l] = out[l] + out[l];
+				else
+					m=1;
 			}
-			
+
 		}
-		return true;
+		if(k==26&&!(m==1))
+			return true;
+		System.out.println("k\t"+k);
+		return false;
 	}
 
 }
